be6b616b9e3e7fc326e03d12c1712acf097251a8
Leon Duif <l.s.duif@gmail.com>
2018-05-24 00:34:06 +0200

    started on spotify api connection

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,10 @@
 const Spotify = {
+    settings: {
+        baseUrl: 'https://api.spotify.com',
+        clientId: '2c5c3e195d8a42e5a080961fbd68bb92',
+        clientSecret: '1dae40764abe4a8d92c2d88212ceb70f'
+    },
+
     search() {
         console.log('searching spotify');
     }
